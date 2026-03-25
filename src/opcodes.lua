08ededc81ed4603be1ad1fc7d066002f1451c230
bradley <131579026+wirlypirly12@users.noreply.github.com>
2026-03-25 18:14:34 -0400

    fixed opcodes, added documentation

--- src/opcodes.lua
+++ src/opcodes.lua
@@ -1,42 +1,62 @@
-local OPCODES = {} -- built at runtime from OPNAMES
-local OPNAMES = { -- every lua5.1 opcode
-	"MOVE",
-	"LOADBOOL",
-	"LOADNIL",
-	"GETUPVAL",
-	"GETTABLE",
-	"GETGLOBAL",
-	"SETGLOBAL",
-	"SETUPVAL",
-	"SETTABLE",
-	"NEWTABLE",
-	"SELF",
+local OPCODES = {}
+local OPNAMES = {
+	-- constants / stack
+	"LOAD_CONST", -- push constants[arg] onto stack
+	"LOAD_LOCAL", -- push locals[arg] onto stack
+	"STORE_LOCAL", -- pop stack into locals[arg]
+	"LOAD_GLOBAL", -- push globals[constants[arg]] onto stack
+	"STORE_GLOBAL", -- pop stack into globals[constants[arg]]
+	"PUSH_NIL", -- push nil
+	"PUSH_TRUE", -- push true
+	"PUSH_FALSE", -- push false
+	"POP", -- discard top of stack
+
+	-- arithmetic
 	"ADD",
 	"SUB",
 	"MUL",
 	"DIV",
 	"MOD",
 	"POW",
-	"UNM",
+	"IDIV",
+	"UNM", -- unary minus
+
+	-- string
+	"CONCAT", -- concat top two stack values
+
+	-- logic
+	"AND",
+	"OR",
 	"NOT",
-	"LEN",
-	"CONCAT",
-	"JMP",
+
+	-- comparison (push true/false onto stack)
 	"EQ",
-	"LE",
+	"NEQ",
 	"LT",
-	"TEST",
-	"TESTSET",
-	"CALL",
-	"TAILCALL",
-	"RETURN",
-	"FORLOOP",
-	"FORPREP",
-	"TFORLOOP",
-	"SETLIST",
-	"CLOSE",
-	"CLOSURE",
-	"VARARG",
+	"LTE",
+	"GT",
+	"GTE",
+	"LEN", -- # operator
+
+	-- control flow
+	"JUMP", -- unconditional jump to arg
+	"JUMP_IF_FALSE", -- pop stack, jump to arg if false
+
+	-- tables
+	"NEW_TABLE", -- push a new empty table
+	"SET_FIELD", -- table[constants[arg]] = pop()
+	"GET_FIELD", -- push table[constants[arg]]
+	"SET_INDEX", -- table[pop()] = pop()
+	"GET_INDEX", -- push table[pop()]
+
+	-- functions
+	"CLOSURE", -- push a new function from chunk[arg]
+	"CALL", -- call function with arg arguments
+	"RETURN", -- return arg values from top of stack
+
+	-- misc
+	"MOVE", -- copy locals[arg1] into locals[arg2]
+	"VARARG", -- push varargs onto stack
 }
 
 for i = 1, #OPNAMES do
