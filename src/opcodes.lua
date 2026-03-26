2d0ddb89db08011ac04821be210a405fce91951c
bradley <131579026+wirlypirly12@users.noreply.github.com>
2026-03-26 19:26:36 -0400

    removed documentation

--- src/opcodes.lua
+++ src/opcodes.lua
@@ -1,19 +1,19 @@
 local OPCODES = {}
 local OPNAMES = {
-	-- constants / stack
-	"LOAD_CONST", -- push constants[arg] onto stack
-	"LOAD_LOCAL", -- push locals[arg] onto stack
-	"STORE_LOCAL", -- pop stack into locals[arg]
-	"LOAD_GLOBAL", -- push globals[constants[arg]] onto stack
-	"LOAD_UPVALUE", -- push upvalue onto stack
-	"STORE_GLOBAL", -- pop stack into globals[constants[arg]]
-	"PUSH_NIL", -- push nil
-	"PUSH_TRUE", -- push true
-	"PUSH_FALSE", -- push false
-	"POP", -- discard top of stack
+
+	"LOAD_CONST",
+	"LOAD_LOCAL",
+	"STORE_LOCAL",
+	"LOAD_GLOBAL",
+	"LOAD_UPVALUE",
+	"STORE_UPVALUE",
+	"STORE_GLOBAL",
+	"PUSH_NIL",
+	"PUSH_TRUE",
+	"PUSH_FALSE",
+	"POP",
 	"CALL_MULTI",
 
-	-- arithmetic
 	"ADD",
 	"SUB",
 	"MUL",
@@ -21,44 +21,40 @@ local OPNAMES = {
 	"MOD",
 	"POW",
 	"IDIV",
-	"UNM", -- unary minus
+	"UNM",
 
-	-- string
-	"CONCAT", -- concat top two stack values
+	"CONCAT",
 
-	-- logic
 	"AND",
 	"OR",
 	"NOT",
 
-	-- comparison (push true/false onto stack)
 	"EQ",
 	"NEQ",
 	"LT",
 	"LTE",
 	"GT",
 	"GTE",
-	"LEN", -- # operator
+	"LEN",
 
-	-- control flow
-	"JUMP", -- unconditional jump to arg
-	"JUMP_IF_FALSE", -- pop stack, jump to arg if false
+	"JUMP",
+	"JUMP_IF_FALSE",
+	"JUMP_IF_FALSE_KEEP",
+	"JUMP_IF_TRUE_KEEP",
 
-	-- tables
-	"NEW_TABLE", -- push a new empty table
-	"SET_FIELD", -- table[constants[arg]] = pop()
-	"GET_FIELD", -- push table[constants[arg]]
-	"SET_INDEX", -- table[pop()] = pop()
-	"GET_INDEX", -- push table[pop()]
+	"NEW_TABLE",
+	"SET_FIELD",
+	"GET_FIELD",
+	"SET_INDEX",
+	"GET_INDEX",
 
-	-- functions
-	"CLOSURE", -- push a new function from chunk[arg]
-	"CALL", -- call function with arg arguments
-	"RETURN", -- return arg values from top of stack
+	"CLOSURE",
+	"CALL",
+	"CALL_VOID",
+	"RETURN",
 
-	-- misc
-	"MOVE", -- copy locals[arg1] into locals[arg2]
-	"VARARG", -- push varargs onto stack
+	"MOVE",
+	"VARARG",
 	"SET_VARARG_TABLE",
 	"VARARG_FIRST",
 }
